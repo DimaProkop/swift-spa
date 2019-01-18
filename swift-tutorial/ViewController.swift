217f9e0faf312558fdfecfe9f36b6188ba711cfa
dzmitry.prakapovich@instinctools.ru <tereras95@gmail.com>
2019-01-18 22:51:34 +0300

    Initial Commit

---
+++ swift-tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-tutorial
+//
+//  Created by Дмитрий Прокопович on 1/18/19.
+//  Copyright © 2019 f.r.i.d.a.y. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
